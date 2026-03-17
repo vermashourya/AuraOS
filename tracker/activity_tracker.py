c1f385bd1371fbee3ede0f74040536bcf1aebb68
Shourya Verma <shouryaverma2006@gmail.com>
2026-03-17 20:14:56 +0530

    Add get_network_status() - real time Wi-Fi , Bluetooth and Ethernet detection

--- tracker/activity_tracker.py
+++ tracker/activity_tracker.py
@@ -1,6 +1,8 @@
 import psutil
+import subprocess
 
 ESSENTIAL_SERVICES = ['WlanSvc','Wcmsvc','WwanSvc','Wcncsvc','bthserv','BluetoothUserService','BTAGService','BthAvctpSvc','Audiosrv','AudioEndpointBuilder','MMCSS','MpsSvc','BFE','SharedAccess','WinDefend','WdNisSvc','Sense','SecurityHealthService','wuauserv','UsoSvc','BITS','DoSvc','WaaSMedicSvc','Dhcp','Dnscache','NlaSvc','Netman','Netprofm','iphlpsvc','lmhosts','WinHttpAutoProxySvc','RasMan','RemoteAccess','FrameServer','FrameServerMonitor','Audiosrv','AudioEndpointBuilder','MMCSS','hidserv','DeviceAssociationService','PlugPlay','SharedAccess','icssvc','WlanSvc','Spooler','PrintNotify']
+
 # This gives the list of all process which are not needed my AuraOS
 def get_blacklist():
     blacklist = [] 
@@ -33,3 +35,26 @@ def get_essential_services():
         if service.name() in ESSENTIAL_SERVICES and service.status() == 'running':
             services.append(service.name())
     return set(services)
+
+# This gives the status of networks connected
+def get_network_status():
+    network = dict({})
+
+    for iface , stat in psutil.net_if_stats().items():
+        if 'Bluetooth' in iface:
+            network['Bluetooth'] = {'status':stat.isup , 'speed':stat.speed}
+        if 'Local Area Connection' in iface:
+            network['Ethernet'] = {'status':stat.isup , 'speed':stat.speed}
+    
+    result = subprocess.check_output(['netsh','wlan','show','interfaces'] , text = True)
+    for line in result.split("\n"):
+        if 'SSID' in line and not 'BSSID' in line:
+            network.setdefault('Wi-Fi' , {}).update({'name':line.split(":")[1].strip()})
+        if 'State' in line:
+            network.setdefault('Wi-Fi' , {}).update({'status':line.split(":")[1].strip()})
+        if 'Receive rate' in line:
+            network.setdefault('Wi-Fi' , {}).update({'speed':line.split(":")[1].strip() + ' Mbps'})
+        if 'Signal' in line:            
+            network.setdefault('Wi-Fi' , {}).update({'signal':line.split(":")[1].strip()})
+    
+    return network         
